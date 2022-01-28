80cf5e03288d16e6d5df53d0c7eab2d7a76a91ee
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-01-28 21:14:19 +0000

    code updated

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,25 @@
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=Birmingham&appid=b8763f38b9f069958af940db06d7fbe0").then(function (res) {
-    return res.json();
-})
-.then(function (data) {
-    console.log(data);
-});
 
-// let weatherFormEl = document.querySelector("#weatherform");
-
-
-// const searchEngine = function (event) {
-//     event.preventDefault();
-
-    
-
-// }
 
 const searchEl = document.getElementById("search");
-const citySearchEl = document.getElementById("searchcity"); 
+const citySearchEl = document.getElementById("searchcity");
+const dayOneEl = document.getElementById("dayone");
+const dayTwoEl = document.getElementById("daytwo");
+const dayThreeEl = document.getElementById("daythree");
+const dayFourEl = document.getElementById("dayfour");
+const dayFiveEl = document.getElementById("dayfive");
+const currentIconId = document.getElementById("currentIcon");
+
 
 searchEl.addEventListener("click", (event)=>{
     event.preventDefault();
     const city = citySearchEl.value;
     console.log(city);
     getCurrentWeather(city);
-
+  
 
 });
+
+
 
 function getCurrentWeather(city){
 console.log(city);
@@ -34,8 +28,10 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b8763f38b
     return res.json();
 })
 .then(function (data) {
-    // console.log(data);
+    const lon = data.coord.lon;
+    const lat = data.coord.lat;
     displayWeather(data);
+    daily(lon, lat);
 });
 
 }
@@ -46,9 +42,19 @@ const currentWeatherId = document.getElementById("currentWeather");
 function displayWeather(weather) {
 console.log(weather);
 
+// Current Icon
+currentIconId.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+currentWeatherId.append(currentIconId);
+
+
 // Current City
 const currentCity = document.createElement("h2");
 currentCity.textContent = "City "+weather.name;
+
+// date
+
+const currentDate = document.createElement("h2");
+currentDate.textContent = "Date "+weather.dt*1000(format("DDDD DDD MMM YY"));
 
 
 
@@ -58,7 +64,7 @@ const tempText = document.createElement("p");
 currentTemp.textContent = "Temp";
 tempText.textContent = weather.main.temp;
 
-// Current City Wind
+// Current City Wind 
 const currentWind = document.createElement("h2");
 const windText = document.createElement("p");
 currentWind.textContent = "Wind";
@@ -70,24 +76,66 @@ const humText = document.createElement("p");
 currentHum.textContent = "Humidity";
 humText.textContent = weather.main.humidity+" %";
 
-// UV Index 
-
-
-currentWeatherId.append(currentCity, currentTemp, tempText, currentWind, windText, currentHum, humText);
+currentWeatherId.append(currentCity, currentTemp, currentDate, tempText, currentWind, windText, currentHum, humText);
 
 }
 
-function forecast() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=Birmingham&appid=b8763f38b9f069958af940db06d7fbe0").then(function (res) {
+const forecastSearch = citySearchEl.value;
+const forecastEl = document.querySelector(".forecast");
+
+function daily(lon, lat) {
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=b8763f38b9f069958af940db06d7fbe0&units=metric`).then(function (res) {
         return res.json();
     })
-    .then(function(data) {
-    
-    for (var i = 0; i < 5;i++)
+    .then(function (data) {
+        console.log(data);
+        displayForecast(data);
+        // UV Index Current
+        const uvIndex = document.createElement("p");
+        uvIndex.textContent=data.current.uvi;
+        currentWeatherId.append(uvIndex);
+  
 
-    });
+})
+}
+function displayForecast(data){
+    const forecast = 5;
+        for (var i=0;i < forecast; i++){
+    const iconImage = document.createElement("img");
+    const column = document.createElement("div");
+    column.setAttribute("class", "col-5");
+    const dailyTemp = document.createElement("p");
+    const dailyHum =document.createElement("p");
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    
+    dailyTemp.setAttribute("class","card-text");
+    dailyHum.setAttribute("class","card-text");
+
+    dailyTemp.textContent = data.daily[i].temp.day;
+    dailyHum.textContent = data.daily[i].humidity;
+    iconImage.setAttribute("src", `http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`)
+    cardBody.append(dailyTemp, dailyHum);
+    card.append(iconImage, cardBody);
+    column.append(card)
+    forecastEl.append(column);
+
+
+    }
+
 
 }
+
+
+
+    
+    
+    
+
+
 
 
 
